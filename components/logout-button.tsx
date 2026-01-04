70daab19594063cae458859a1560ed549ba679a6
theDahunsiDavid <gbemigadare@gmail.com>
2026-01-04 19:58:20 +0100

    feat: Create reusable logout button component.

---
+++ components/logout-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { createClient } from "@/lib/supabase/client";
+import { useRouter } from "next/navigation";
+
+export function LogoutButton() {
+  const router = useRouter();
+  const supabase = createClient();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.push("/auth/login");
+  };
+
+  return (
+    <button onClick={handleLogout} className="">
+      Logout
+    </button>
+  );
+}
